830084231c51b77d3ed177ca947de682566ae678
zsl <zsl@smdt.com.cn>
2014-05-20 10:30:26 +0800

    add some ignore files

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 8 */
+/* This file is auto generated, version 4 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#8 SMP PREEMPT Mon Apr 14 13:21:51 CST 2014"
+#define UTS_VERSION "#4 SMP PREEMPT Tue May 20 09:55:49 CST 2014"
 #define LINUX_COMPILE_BY "zsl"
 #define LINUX_COMPILE_HOST "Server-102"
 #define LINUX_COMPILER "gcc version 4.6.3 20120201 (prerelease) (crosstool-NG linaro-1.13.1-2012.02-20120222 - Linaro GCC 2012.02) "
--- modules/example/.tmp_versions/example.mod
+++ modules/example/.tmp_versions/example.mod
@@ -1,2 +1,2 @@
-/home/zsl/a20-v12/lichee/linux-3.3/modules/example/example.ko
-/home/zsl/a20-v12/lichee/linux-3.3/modules/example/example.o
+/home/zsl/A20ArmCore_V12/lichee/linux-3.3/modules/example/example.ko
+/home/zsl/A20ArmCore_V12/lichee/linux-3.3/modules/example/example.o
